c6aed4df776d12aad0378146c9ba328e2485ba1f
alvaro <alvaro.apariciofdz@gmail.com>
2024-07-31 22:15:38 +0200

    added login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,16 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { COMPANY_NAME } from './conts/CONTS';
-import Login from './Login';
-
-function Home() {
-  return (
-    <div>
-      <h1 className='text-4xl font-bold text-blue-700 mb-4'>
-        Bienvenido a {`${COMPANY_NAME}`}
-      </h1>
-      <section className='p-8 max-w-4xl w-full'>
-        <h3 className='text-2xl font-semibold text-yellow-300 mb-6'>
-          {`${COMPANY_NAME} `} es una aplicación de gestión financiera diseñada
-          para ayudarte a mantener un control detallado de tus ingresos y
-          gastos. Con una interfaz intuitiva y moderna, puedes:
-        </h3>
-        <ul className='list-disc pl-6 space-y-4 text-gray-300 text-left'>
-          <li className='text-lg'>
-            <p>
-              <b>Gestionar Usuarios:</b> Crear y administrar tu perfil,
-              incluyendo detalles personales y financieros.
-            </p>
-          </li>
-          <li className='text-lg'>
-            <p>
-              <b>Registrar Transacciones:</b> Añadir, editar y eliminar
-              transacciones, clasificándolas en categorías como ingresos o
-              gastos.
-            </p>
-          </li>
-          <li className='text-lg'>
-            <p>
-              <b>Organizar Categorías:</b> Definir y gestionar categorías para
-              tus transacciones, facilitando un análisis financiero más
-              detallado.
-            </p>
-          </li>
-        </ul>
-      </section>
-    </div>
-  );
-}
+import Home from './components/Home';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,106 @@
+import { COMPANY_NAME } from '../../conts/CONTS';
+
+export default function Login() {
+  return (
+    <div className='bg-white dark:bg-zinc-900'>
+      <div className='flex justify-center h-screen'>
+        <aside
+          className='hidden bg-cover lg:block lg:w-2/3'
+          style={{
+            backgroundImage: `url(/background.jpg)`,
+          }}
+        >
+          <div className='flex items-center h-full px-32'>
+            <div className='text-left'>
+              <h2 className='text-6xl font-bold text-white'>{COMPANY_NAME}</h2>
+
+              <p className='max-w-xl mt-3 text-gray-300'>
+                Bienvenido a {COMPANY_NAME}, tu plataforma confiable para
+                gestionar y controlar tus finanzas personales. Aquí, puedes
+                realizar un seguimiento de tus gastos, planificar tu presupuesto
+                y alcanzar tus objetivos financieros con facilidad.
+              </p>
+            </div>
+          </div>
+        </aside>
+
+        <main className='flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6 text-left'>
+          <div className='flex-1'>
+            <header className='text-center'>
+              <h1 className='text-4xl font-bold text-center text-gray-700 dark:text-white'>
+                {COMPANY_NAME}
+              </h1>
+
+              <p className='mt-3 text-gray-500 dark:text-gray-300'>
+                Inicia sesión para acceder a tu cuenta y tomar el control de tus
+                finanzas.
+              </p>
+            </header>
+
+            <section className='mt-8'>
+              <form>
+                <div>
+                  <label
+                    htmlFor='email'
+                    className='block mb-2 text-sm text-gray-600 dark:text-gray-200'
+                  >
+                    Dirección de Email
+                  </label>
+                  <input
+                    type='email'
+                    name='email'
+                    id='email'
+                    placeholder='example@example.com'
+                    className='block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-zinc-950 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+                  />
+                </div>
+
+                <div className='mt-6'>
+                  <div className='flex justify-between mb-2'>
+                    <label
+                      htmlFor='password'
+                      className='text-sm text-gray-600 dark:text-gray-200'
+                    >
+                      Contraseña
+                    </label>
+                    <a
+                      href='#'
+                      className='text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline'
+                    >
+                      ¿Olvidó su contraseña?
+                    </a>
+                  </div>
+
+                  <input
+                    type='password'
+                    name='password'
+                    id='password'
+                    placeholder='Tu contraseña'
+                    className='block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-zinc-950 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40'
+                  />
+                </div>
+
+                <div className='mt-6'>
+                  <button className='w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50'>
+                    Iniciar Sesión
+                  </button>
+                </div>
+              </form>
+
+              <p className='mt-6 text-sm text-center text-gray-400'>
+                ¿No tienes una cuenta todavía?{' '}
+                <a
+                  href='#'
+                  className='text-blue-500 focus:outline-none focus:underline hover:underline'
+                >
+                  Regístrate
+                </a>
+                .
+              </p>
+            </section>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
