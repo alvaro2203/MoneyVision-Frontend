72c09e61a868572360dc402010ee14dec7d207d0
OscarGE11 <geoscar1100@gmail.com>
2024-09-04 22:14:46 +0200

    added token management

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, useNavigate } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login/Login';
+import Cookies from 'js-cookie';
+import { useEffect } from 'react';
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = Cookies.get('access_token');
+
+    if (token) {
+      navigate('/');
+    } else {
+      navigate('/login');
+    }
+  }, [navigate]);
+
+
   return (
     <BrowserRouter>
       <Routes>
