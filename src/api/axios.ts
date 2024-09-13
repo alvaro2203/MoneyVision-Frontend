20e5638e9c03d127c3b52342dd948d81f5671c8c
OscarGE11 <geoscar1100@gmail.com>
2024-09-13 13:16:53 +0200

    fixed token login

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_API_URL,
-  withCredentials: true, 
+  withCredentials: true,
 });
 
 export default api;
