0221aaaaeab50cba0caf32d35b1a149e0105e0b5
alvaro <alvaro.apariciofdz@gmail.com>
2024-09-21 13:40:02 +0200

    changed styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -49,10 +49,10 @@ export default function Home() {
   if (!user) return <p>No se pudo cargar el usuario.</p>;
 
   return (
-    <div className='container mx-auto px-4 py-8'>
+    <div className='container mx-auto px-4 py-8 space-y-20 max-w-4xl'>
       <h1 className='text-3xl font-bold mb-8'>¡Bienvenido, {user.username}!</h1>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-8'>
         <Card
           className={`${
             user.money !== null &&
@@ -81,14 +81,12 @@ export default function Home() {
         <Card>
           <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
             <CardTitle className='text-sm font-medium'>
-              Ingresos Totales
+              Número de Transacciones
             </CardTitle>
-            <ArrowUpDown className='h-4 w-4 text-muted-foreground' />
+            <BarChart3 className='h-4 w-4 text-muted-foreground' />
           </CardHeader>
           <CardContent>
-            <div className='text-2xl font-bold'>
-              {formatCurrency(totalIncomes)}
-            </div>
+            <div className='text-2xl font-bold'>{user.transactions.length}</div>
           </CardContent>
         </Card>
 
@@ -116,126 +114,128 @@ export default function Home() {
         <Card>
           <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
             <CardTitle className='text-sm font-medium'>
-              Número de Transacciones
+              Ingresos Totales
             </CardTitle>
-            <BarChart3 className='h-4 w-4 text-muted-foreground' />
+            <ArrowUpDown className='h-4 w-4 text-muted-foreground' />
           </CardHeader>
           <CardContent>
-            <div className='text-2xl font-bold'>{user.transactions.length}</div>
+            <div className='text-2xl font-bold'>
+              {formatCurrency(totalIncomes)}
+            </div>
           </CardContent>
         </Card>
       </div>
 
-      <div className='flex justify-between items-center mb-8'>
-        <h2 className='text-2xl font-bold'>Transacciones</h2>
-        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-          <DialogTrigger asChild>
-            <Button>Añadir Transacción</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Añadir Nueva Transacción</DialogTitle>
-            </DialogHeader>
-            <form onSubmit={handleSubmit} className='space-y-4'>
-              <div>
-                <Label htmlFor='title'>Título</Label>
-                <Input
-                  id='title'
-                  name='title'
-                  value={newTransaction.title}
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-              <div>
-                <Label htmlFor='description'>Descripción</Label>
-                <Input
-                  id='description'
-                  name='description'
-                  value={newTransaction.description}
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div>
-                <Label htmlFor='amount'>Cantidad</Label>
-                <Input
-                  id='amount'
-                  name='amount'
-                  type='number'
-                  step='0.01'
-                  value={newTransaction.amount || ''}
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-              <div>
-                <Label htmlFor='typeOfTransaction'>Tipo de Transacción</Label>
-                <Select
-                  name='typeOfTransaction'
-                  onValueChange={(value) =>
-                    handleSelectChange('typeOfTransaction', value)
-                  }
-                  required
-                >
-                  <SelectTrigger>
-                    <SelectValue placeholder='Seleccionar tipo' />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value={TYPE_OF_TRANSACTION_INCOME}>
-                      {TYPE_OF_TRANSACTION_ENUM.Income}
-                    </SelectItem>
-                    <SelectItem value={TYPE_OF_TRANSACTION_EXPENSE}>
-                      {TYPE_OF_TRANSACTION_ENUM.Expense}
-                    </SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-              <div>
-                <Label htmlFor='category'>Categoría</Label>
-                <Select
-                  name='category'
-                  value={newTransaction.category._id}
-                  onValueChange={(value) =>
-                    handleSelectChange('category', value)
-                  }
-                  required
-                >
-                  <SelectTrigger>
-                    <SelectValue placeholder='Selecciona una categoría' />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {categories.map((category) => (
-                      <SelectItem key={category._id} value={category._id}>
-                        {category.name}
+      <div className='w-full mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex justify-between items-center p-6'>
+          <CardHeader className='p-0'>
+            <CardTitle className='text-2xl font-bold'>
+              Transacciones Recientes
+            </CardTitle>
+          </CardHeader>
+          <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <DialogTrigger asChild>
+              <Button>Añadir Transacción</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Añadir Nueva Transacción</DialogTitle>
+              </DialogHeader>
+              <form onSubmit={handleSubmit} className='space-y-4'>
+                <div>
+                  <Label htmlFor='title'>Título</Label>
+                  <Input
+                    id='title'
+                    name='title'
+                    value={newTransaction.title}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <Label htmlFor='description'>Descripción</Label>
+                  <Input
+                    id='description'
+                    name='description'
+                    value={newTransaction.description}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div>
+                  <Label htmlFor='amount'>Cantidad</Label>
+                  <Input
+                    id='amount'
+                    name='amount'
+                    type='number'
+                    step='0.01'
+                    value={newTransaction.amount || ''}
+                    onChange={handleInputChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <Label htmlFor='typeOfTransaction'>Tipo de Transacción</Label>
+                  <Select
+                    name='typeOfTransaction'
+                    onValueChange={(value) =>
+                      handleSelectChange('typeOfTransaction', value)
+                    }
+                    required
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder='Seleccionar tipo' />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value={TYPE_OF_TRANSACTION_INCOME}>
+                        {TYPE_OF_TRANSACTION_ENUM.Income}
                       </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </div>
-              {transactionError && (
-                <p className='text-red-500'>{transactionError}</p>
-              )}
-              <Button type='submit'>Guardar Transacción</Button>
-            </form>
-          </DialogContent>
-        </Dialog>
-      </div>
-
-      <Card className='mb-8'>
-        <CardHeader>
-          <CardTitle></CardTitle>
-        </CardHeader>
-        <CardContent>
+                      <SelectItem value={TYPE_OF_TRANSACTION_EXPENSE}>
+                        {TYPE_OF_TRANSACTION_ENUM.Expense}
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div>
+                  <Label htmlFor='category'>Categoría</Label>
+                  <Select
+                    name='category'
+                    value={newTransaction.category._id}
+                    onValueChange={(value) =>
+                      handleSelectChange('category', value)
+                    }
+                    required
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder='Selecciona una categoría' />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {categories.map((category) => (
+                        <SelectItem key={category._id} value={category._id}>
+                          {category.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+                {transactionError && (
+                  <p className='text-red-500'>{transactionError}</p>
+                )}
+                <Button type='submit'>Guardar Transacción</Button>
+              </form>
+            </DialogContent>
+          </Dialog>
+        </div>
+        <CardContent className='pt-6'>
           <TransactionList
             transactions={user.transactions}
             handleDelete={handleDelete}
             formatCurrency={formatCurrency}
           />
-          <div className='mt-4 text-center'>
+          <div className='mt-6 text-center'>
             <Button variant='outline'>Ver Todas las Transacciones</Button>
           </div>
         </CardContent>
-      </Card>
+      </div>
     </div>
   );
 }
